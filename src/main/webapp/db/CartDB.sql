1b21a2b73433fb12622dbe0ae70b06c210a6989c
site4 <site4@LIMBOOK>
2024-02-23 19:51:08 +0900

    cartdb sql 수정

--- src/main/webapp/db/CartDB.sql
+++ src/main/webapp/db/CartDB.sql
@@ -6,11 +6,20 @@ CREATE TABLE `Cart` (
 	`prod_count`	int	NOT NULL
 );
 
-ALTER TABLE `Cart` ADD CONSTRAINT `PK_CART` PRIMARY KEY (
-	`cart_idx`
+ALTER TABLE `cart` ADD CONSTRAINT `FK_user_TO_cart_1` FOREIGN KEY (
+	`user_idx`
+)
+REFERENCES `user` (
+	`user_idx`
 );
 
 select * from cart;
--- 샘플 데이터
+-- 샘플 데이터 (user 테이블에 회원정보가 존재해야 함)
 INSERT INTO cart (`cart_idx`, `user_idx`, `prod_idx`, `prod_count`) VALUES ('1', '1', '1', '1');
 commit;
+
+
+
+select  ifnull(sum(p.p_price),0) 
+from cart c join product p on  p.p_idx=c.prod_idx
+where c.user_idx=1;
