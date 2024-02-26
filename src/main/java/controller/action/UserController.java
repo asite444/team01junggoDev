d25d0961b2a38a496b164c2734e90d3667bc6930
user <user@BOOK-UP5F0RS3G7>
2024-02-26 18:19:44 +0900

    유저 컨트롤러 수정- 상균

--- src/main/java/controller/action/UserController.java
+++ src/main/java/controller/action/UserController.java
@@ -1,7 +1,12 @@
 package controller.action;
 
+import java.io.IOException;
 import java.util.List;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -101,10 +106,73 @@ public class UserController {
 		return "redirect:../main.jsp";
 	
 	  }//end: logout
-	
+	  
+	  
+	  // 회원가입 폼
+	  @WebServlet("/user/insert_form.do")
+	  public class MemberInsertFormAction extends HttpServlet {
+	  	private static final long serialVersionUID = 1L;
 
-		
-	}
+	  	/**
+	  	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	  	 */
+	  	protected void service(HttpServletRequest request, HttpServletResponse response)
+	  			throws ServletException, IOException {
+	  		// TODO Auto-generated method stub
+	  		
+
+	  		//출력부분을 jsp에게 Dispatcher(forward)시킨다
+	  		String forward_page = "user_insert_form.jsp";
+	  		RequestDispatcher disp = request.getRequestDispatcher(forward_page);
+	  		disp.forward(request, response);
+
+	  	}	  
+	  
+	 }//end: insert_form.do
+	  
+	  
+	  // 회원가입
+	  @WebServlet("/user/insert.do")
+	  public class MemberInsertAction extends HttpServlet {
+	  	private static final long serialVersionUID = 1L;
+
+	  	/**
+	  	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	  	 */
+	  	protected void service(HttpServletRequest request, HttpServletResponse response)
+	  			throws ServletException, IOException {
+	  		// TODO Auto-generated method stub
+	  		
+
+	  		// /member/insert.do?mem_name=박길동&mem_id=park&mem_pwd=1234&mem_email=a@b.com&
+	  		//                   mem_zipcode=06267&mem_addr=서울 강남구 강남대로 238
+
+	  		//1.paramter 받기
+	  		String user_name	=	request.getParameter("user_name");
+	  		String user_id		=	request.getParameter("user_id");
+	  		String user_pwd		=	request.getParameter("user_pwd");
+	  		String user_email	=	request.getParameter("user_email");
+	  		String user_zipcode	=	request.getParameter("user_zipcode");
+	  		String user_addr	=	request.getParameter("user_addr");
+	  		
+	  		//2.IP
+	  		String user_ip		=	request.getRemoteAddr();
+	  		
+	  		//3.VO포장
+	  		UserVo vo = new UserVo(user_name, user_id, user_pwd, user_email, user_zipcode, user_addr, user_ip);
+	  		
+	  		//4.DB insert
+	  		int res = UserDao.getInstance().insert(vo);
+	  		
+	  		//5.목록보기(차후:로그인창 이동)
+	  		response.sendRedirect("../main.jsp");
+	  		
+	  	 }
+	  	
+	  }//end: insert.do
+
+
+}
 	
 	
 
